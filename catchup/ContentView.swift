d194e30b4a8e482236ae37535c20b9dd4cbe979b
Katherine Wong <kmwong@ucsd.edu>
2022-04-09 10:51:39 -0700

    Initial Commit

---
+++ catchup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  catchup
+//
+//  Created by Kat Kat on 4/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ catchup/catchupApp.swift
@@ -0,0 +1,17 @@
+//
+//  catchupApp.swift
+//  catchup
+//
+//  Created by Kat Kat on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct catchupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
