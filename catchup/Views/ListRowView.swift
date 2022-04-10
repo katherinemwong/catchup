83f25836e8eaac2f8de27cebf72904dc78f6bbbc
John Adams <jpadams@ucsd.edu>
2022-04-10 02:33:47 -0700

    fixed listrowview color

--- catchup/Views/ListRowView.swift
+++ catchup/Views/ListRowView.swift
@@ -26,7 +26,7 @@ struct ListRowView: View {
                 Spacer()
                 Text("contact every \(person.frequency) days")
             }
-            .foregroundColor(Color.white)
+            .foregroundColor(Color("darkBlue"))
             .cornerRadius(8)
         }
     }
