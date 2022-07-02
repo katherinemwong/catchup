e9a4b2aa54c590d19aefccc3bd8020735a525915
John Adams <jpadams@ucsd.edu>
2022-07-02 13:51:58 -0700

    Added new list identifier to personModel, and added comments

--- catchup/Models/personModel.swift
+++ catchup/Models/personModel.swift
@@ -2,14 +2,15 @@
 //  personModel.swift
 //  catchup
 //
-//  Created by Kat Kat on 4/9/22.
+//  John & Katherine
 //
 
 import Foundation
 
 struct personModel: Identifiable, Hashable, Codable {
-    let name: String
-    let dateToContact: Date
-    let frequency: Int
-    var id: String = UUID().uuidString
+    let name: String                        //the name of the person
+    let dateToContact: Date                 //next time to contact the person
+    let frequency: Int                      //how often to contact the person
+    var id: String = UUID().uuidString      //a unique identifier for the person
+    let parentList: String                  //the list that this person belongs in
 }
